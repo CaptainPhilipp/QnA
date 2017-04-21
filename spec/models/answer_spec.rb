21efecda9e1923edaa0bfc4d950a80841aa37fe0
CaptainPhilipp <Ph-s@mail.ru>
2017-04-22 01:07:03 +0300

    presence of question

--- spec/models/answer_spec.rb
+++ spec/models/answer_spec.rb
@@ -3,6 +3,7 @@ require 'rails_helper'
 RSpec.describe Answer, type: :model do
   it { should belong_to(:question) }
   it { should have_db_index(:question_id) }
+  it { should validate_presence_of(:question_id) }
 
   it { should validate_length_of(:body).is_at_least(6) }
 end
