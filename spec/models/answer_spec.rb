9e496ee1cb2f5a906c5b36eaa092355563347c50
CaptainPhilipp <Ph-s@mail.ru>
2017-04-22 00:42:33 +0300

    Question, Answer

---
+++ spec/models/answer_spec.rb
@@ -0,0 +1,8 @@
+require 'rails_helper'
+
+RSpec.describe Answer, type: :model do
+  it { should belong_to(:question) }
+  it { should have_db_index(:question_id) }
+
+  it { should validate_length_of(:body).is_at_least(6) }
+end
