37360f8e66b47771f695bde3e1bd863ea8168536
CaptainPhilipp <Ph-s@mail.ru>
2017-04-22 19:34:58 +0300

    Questions :new :show

--- spec/controllers/questions_controller_spec.rb
+++ spec/controllers/questions_controller_spec.rb
@@ -3,9 +3,9 @@ require 'rails_helper'
 RSpec.describe QuestionsController, type: :controller do
   describe 'GET #index' do
     before { get :index }
+    let(:questions) { create_list :question, 2 + rand(5) }
 
     it 'populates an array of questions' do
-      questions = FactoryGirl.create_list :question, 2 + rand(5)
       expect(assigns :questions).to match_array(questions)
     end
 
@@ -13,4 +13,27 @@ RSpec.describe QuestionsController, type: :controller do
       expect(response).to render_template(:index)
     end
   end
+
+  describe 'GET #show' do
+    before { get :show, params: { id: question } }
+    let(:question) { create :question }
+
+    it 'assigns requested question to @question' do
+      expect(assigns :question).to eq(question)
+    end
+
+    it { should render_template(:show) }
+  end
+
+  describe 'GET #new' do
+    before { get :new }
+    let(:question) { build :question }
+
+    it 'assigns a new Question to @question' do
+      expect(assigns :question).to be_a_new(Question)
+    end
+
+    it { should render_template(:new) }
+  end
+
 end
