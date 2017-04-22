0b3d2fa4765e4a19a555e18b534cef5647d03621
CaptainPhilipp <Ph-s@mail.ru>
2017-04-23 01:37:26 +0300

    AnswersController#create + rspec

--- app/controllers/answers_controller.rb
+++ app/controllers/answers_controller.rb
@@ -4,8 +4,25 @@ class AnswersController < ApplicationController
   end
 
   def new
+    @answer = Answer.new
+  end
+
+  def create
+    @answer = Answer.new(answers_params)
+    if @answer.save
+      redirect_to @answer
+    else
+      render :new
+    end
   end
 
   def edit
+  end
+
+  private
+
+  def answers_params
+    params.require(:answer).permit(:body)
+      .merge(question_id: params[:question_id])
   end
 end
--- spec/controllers/answers_controller_spec.rb
+++ spec/controllers/answers_controller_spec.rb
@@ -3,9 +3,10 @@ require 'rails_helper'
 RSpec.describe AnswersController, type: :controller do
   let(:question) { create :question }
   let(:answer)   { create :answer, question: question }
+  let(:with_question) { { question_id: question } }
 
   describe 'GET #index' do
-    before { get :index, params: { question_id: question } }
+    before { get :index, params: with_question }
     let(:answers) { create_list :answer, 2 + rand(5), question: question }
 
     it 'populates an array of answers' do
@@ -16,45 +17,45 @@ RSpec.describe AnswersController, type: :controller do
       should render_template :index
     end
   end
-  #
-  # describe 'GET #new' do
-  #   before { get :new }
-  #   let(:answer) { build :answer }
-  #
-  #   it 'assigns a new Answer to @answer' do
-  #     expect(assigns :answer).to be_a_new(Answer)
-  #   end
-  #
-  #   it { should render_template :new }
-  # end
-  #
-  # describe 'POST #create' do
-  #   let(:send_request) { post :create, params: answer_params }
-  #
-  #   context 'with valid attrs' do
-  #     let(:answer_params) { { answer: attributes_for(:answer) } }
-  #
-  #     it 'saves new answer' do
-  #       expect { send_request }.to change(Answer, :count)
-  #     end
-  #
-  #     it 'redirects to new answer' do
-  #       send_request
-  #       should redirect_to answer_path(assigns :answer)
-  #     end
-  #   end
-  #
-  #   context 'with invalid attrs' do
-  #     let(:answer_params) { { answer: attributes_for(:invalid_answer) } }
-  #
-  #     it 'does not save the answer' do
-  #       expect { send_request }.to_not change(Answer, :count)
-  #     end
-  #
-  #     it 're-renders new view' do
-  #       send_request
-  #       should render_template :new
-  #     end
-  #   end
-  # end
+
+  describe 'GET #new' do
+    before { get :new, params: with_question }
+    let(:answer) { build :answer }
+
+    it 'assigns a new Answer to @answer' do
+      expect(assigns :answer).to be_a_new(Answer)
+    end
+
+    it { should render_template :new }
+  end
+
+  describe 'POST #create' do
+    let(:send_request) { post :create, params: answer_params }
+
+    context 'with valid attrs' do
+      let(:answer_params) { { question_id: question.id }.merge answer: attributes_for(:answer) }
+
+      it 'saves new answer' do
+        expect { send_request }.to change(question.answers, :count)
+      end
+
+      it 'redirects to new answer' do
+        send_request
+        should redirect_to answer_path(assigns :answer)
+      end
+    end
+
+    # context 'with invalid attrs' do
+    #   let(:answer_params) { { answer: attributes_for(:invalid_answer) } }
+    #
+    #   it 'does not save the answer' do
+    #     expect { send_request }.to_not change(Answer, :count)
+    #   end
+    #
+    #   it 're-renders new view' do
+    #     send_request
+    #     should render_template :new
+    #   end
+    # end
+  end
 end
