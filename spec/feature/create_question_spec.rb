bd09eb33ddf8191692235966d5ab57d7a9daebc1
CaptainPhilipp <Ph-s@mail.ru>
2017-04-26 00:14:31 +0300

    macros

--- spec/feature/create_question_spec.rb
+++ spec/feature/create_question_spec.rb
@@ -14,10 +14,7 @@ feature 'create question', %q(
 
   scenario 'after create, user can see his question' do
     new_question_from_index
-    fill_in Question.human_attribute_name(:title), with: question[:title]
-    fill_in Question.human_attribute_name(:body),  with: question[:body]
-    click_on I18n.t(:create, scope: 'questions.form')
-
+    create_question_with_form
     expect(page).to have_content question[:title]
     expect(page).to have_content question[:body]
   end
--- spec/support/questions_macros.rb
+++ spec/support/questions_macros.rb
@@ -3,4 +3,12 @@ module QuestionsMacros
     visit questions_path
     click_link I18n.t('questions.index.create')
   end
+
+  def create_question_with_form(attributes = nil)
+    attributes ||= { title: 'Question title', body: 'Question body' }
+
+    fill_in Question.human_attribute_name(:title), with: attributes[:title]
+    fill_in Question.human_attribute_name(:body),  with: attributes[:body]
+    click_on I18n.t(:create, scope: 'questions.form')
+  end
 end
