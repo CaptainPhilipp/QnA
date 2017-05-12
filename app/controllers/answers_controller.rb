0a1dc7e7174f348e0c7275d4569d2a4827d86633
CaptainPhilipp <Ph-s@mail.ru>
2017-05-13 00:53:58 +0300

    prepare for new ajax func

--- app/controllers/answers_controller.rb
+++ app/controllers/answers_controller.rb
@@ -8,7 +8,7 @@ class AnswersController < ApplicationController
     @answer = @question.answers.new(answers_params)
     @answer.user = current_user
     respond_to do |format|
-      format.js   { render @answer.save ? 'create' : 'errors' }
+      format.js   { render 'create' }
       format.html { @answer.save ? redirect_to(@question) : render('questions/show') }
     end
   end
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,4 +2,8 @@ module ApplicationHelper
   def current_user_owns?(entity)
     current_user && current_user.owner?(entity)
   end
+
+  def css_id_for(entity)
+    "#{entity.class.to_s.tableize}-#{entity.id || 'new'}"
+  end
 end
