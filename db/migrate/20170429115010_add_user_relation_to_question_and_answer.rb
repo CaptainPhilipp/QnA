66a4ee16703af477cdd25743c79f3a0e5ac96e67
CaptainPhilipp <Ph-s@mail.ru>
2017-05-17 19:16:15 +0300

    scheme clear

--- db/migrate/20170429115010_add_user_relation_to_question_and_answer.rb
+++
@@ -1,6 +0,0 @@
-class AddUserRelationToQuestionAndAnswer < ActiveRecord::Migration[5.0]
-  def change
-    add_reference :questions, :user, foreign_key: true, null: false
-    add_reference :answers, :user, foreign_key: true, null: false
-  end
-end
--- db/migrate/20170514120525_add_best_to_answers.rb
+++ db/migrate/20170514120525_add_best_to_answers.rb
@@ -1,6 +1,11 @@
 class AddBestToAnswers < ActiveRecord::Migration[5.0]
-  def change
-    add_column :answers, :best, :bool
+  def up
+    add_column :answers, :best, :bool, null: false, default: false
     add_index :answers, [:question_id, :best]
+  end
+
+  def down
+    remove_column :answers, :best
+    remove_index :answers, [:question_id, :best]
   end
 end
