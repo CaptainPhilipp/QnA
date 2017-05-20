0f5ecb19cd17aa8c0a8c1e245f0f0512190f1b1c
CaptainPhilipp <Ph-s@mail.ru>
2017-05-20 16:02:32 +0300

    Spec for a few attachments to answer

--- spec/feature/answer_attachment_spec.rb
+++ spec/feature/answer_attachment_spec.rb
@@ -9,6 +9,8 @@ feature 'Attach files to answer', "
   let(:attributes) { attributes_for :answer }
   let(:question)   { create :question }
 
+  let(:file_field_name) { Attachment.human_attribute_name(:file) }
+
   login_user
 
   before { visit question_path(question) }
@@ -17,12 +19,37 @@ feature 'Attach files to answer', "
     within 'form#new_answer' do
       fill_in Answer.human_attribute_name(:body),  with: attributes[:body]
 
-      attach_file 'Выбрать файл', "#{Rails.root}/spec/rails_helper.rb"
+      click_link Attachment.human_attribute_name(:add)
+      wait_for_ajax
+
+      attach_file file_field_name,
+        "#{Rails.root}/spec/rails_helper.rb"
       click_on I18n.t(:create, scope: 'answers.form')
     end
 
     within '#answers' do
       expect(page).to have_link 'rails_helper.rb', href: '/uploads/attachment/file/1/rails_helper.rb'
+    end
+  end
+
+  scenario 'User adds a few attachments to answer', js: true do
+    within 'form#new_answer' do
+      fill_in Answer.human_attribute_name(:body),  with: attributes[:body]
+      (0..3).each { |i| click_link Attachment.human_attribute_name(:add) }
+
+      all('form .nested-fields').each do |a|
+        within a do
+          attach_file file_field_name, "#{Rails.root}/spec/uploads/#{i}_test.rb"
+        end
+      end
+
+      click_on I18n.t(:create, scope: 'answers.form')
+
+      within '#answers' do
+        Answer.last.attachments.each do |a|
+          expect(page).to have_link a.file.identifier, href: a.file.url
+        end
+      end
     end
   end
 end
