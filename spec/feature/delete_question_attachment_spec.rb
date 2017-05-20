c82c75537e11054332cb6ce5d40f705d1ceb124d
CaptainPhilipp <Ph-s@mail.ru>
2017-05-20 20:56:38 +0300

    Feature Delete Question Attachment

---
+++ spec/feature/delete_question_attachment_spec.rb
@@ -0,0 +1,20 @@
+require_relative 'acceptance_helper'
+
+feature 'Delete attachments from question' do
+  login_user
+  let(:question)    { create :question, user: user }
+  let!(:attachment) { create :attachment, attachable: question }
+
+  let(:model_name) { I18n.t(:question, scope: 'activerecord.models', count: 1) }
+
+  background { visit edit_question_path(question) }
+
+  scenario 'User deletes attachment' do
+    within "#question" do
+      click_link Attachment.human_attribute_name(:delete)
+      click_button I18n.t(:update, scope: 'helpers.submit', model: model_name)
+
+      expect(page).to_not have_link attachment.file.identifier, href: attachment.file.url
+    end
+  end
+end
