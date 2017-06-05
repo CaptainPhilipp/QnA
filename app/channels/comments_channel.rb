ff71d25f048fc5edf7a9e02b458b5d29dca7e807
CaptainPhilipp <CaptainPhilipp@users.noreply.github.com>
2017-06-05 12:33:38 +0300

    lil fix

--- app/channels/comments_channel.rb
+++ app/channels/comments_channel.rb
@@ -2,6 +2,6 @@ class CommentsChannel < ApplicationCable::Channel
   def subscribed
     type = params[:commentable_type]
     id   = params[:commentable_id]
-    stream_for "comment/#{type}#{id}"
+    stream_from "comment/#{type}#{id}"
   end
 end
