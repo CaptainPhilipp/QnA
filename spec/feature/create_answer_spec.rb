ec3c577c100ff7e47da6696abc59d6c9c1282ff2
CaptainPhilipp <Ph-s@mail.ru>
2017-06-05 08:48:03 +0300

    wait for ajax

--- spec/feature/create_answer_spec.rb
+++ spec/feature/create_answer_spec.rb
@@ -50,6 +50,8 @@ feature 'Create answer', '
         fill_in Answer.human_attribute_name(:body), with: attributes[:body]
         click_button I18n.t(:create, scope: 'answers.form')
 
+        wait_for_ajax
+        
         within "#answer_#{question.answer_ids.last}" do
           expect(page).to have_content attributes[:body]
         end
