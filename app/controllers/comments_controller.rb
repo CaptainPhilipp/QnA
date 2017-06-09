2a773b262592565302f2de69a4dff89c131db819
CaptainPhilipp <Ph-s@mail.ru>
2017-06-10 02:04:46 +0300

    some refactor

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -14,6 +14,7 @@ class CommentsController < ApplicationController
     id   = commentable.id
     CommentsChannel.broadcast_to "comment/#{type}#{id}",
       ApplicationController.render(json: @comment)
+      ApplicationController.render(json: @comment.serialize_to_broadcast)
   end
 
   def comment_params
