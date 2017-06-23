942002504bacae736fb3c71b2d3ebfb44a6ab0cd
CaptainPhilipp <Ph-s@mail.ru>
2017-06-23 09:27:09 +0300

    Oauth controller

--- app/controllers/omniauth_callbacks_controller.rb
+++ app/controllers/omniauth_callbacks_controller.rb
@@ -1,4 +1,7 @@
 class OmniauthCallbacksController < ApplicationController
   def facebook
+    @user = OauthUserAuthorization.new(request.env['omniauth.auth']).call
+    set_flash_message(:notice, :success, kind: 'Facebook') if is_navigational_format?
+    sign_in_and_redirect @user, event: :authorization
   end
 end
