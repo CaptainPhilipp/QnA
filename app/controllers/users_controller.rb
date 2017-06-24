1e614b87228893cfe1d3e1edeb0f5a2b40d9e688
CaptainPhilipp <Ph-s@mail.ru>
2017-06-24 20:08:33 +0300

    in action - if-else to case

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,30 +2,23 @@ class UsersController < ApplicationController
   skip_before_action :authenticate_user!, only: %i(email)
 
   def email
-    @user = OauthUserService.from_session(auth_params, session)
-
-    if @user.valid? && @user.confirmed?
-      sign_in_and_redirect(user, event: :authorization)
-
-    elsif @user.email_taken_error?
-      when_email_taken
-
-    elsif @user.valid? && !@user.confirmed?
-      when_successful_registered
-    else
-      render('email')
+    case @user = OauthUserService.from_session(auth_params, session)
+    when proc(&:valid?), proc(&:confirmed?)  then sign_in_and_redirect(user, event: :authorization)
+    when proc(&:valid?), !proc(&:confirmed?) then when_successful_registered
+    when proc(&:email_taken_error?)          then when_email_taken
+    else render('email')
     end
   end
 
   private
 
-  def when_email_taken
-    flash[:notice] = 'User with this email already registered. Please try to login'
+  def when_successful_registered
+    flash[:notice] = 'User registered! Please, confirm email'
     redirect_to new_user_session_path
   end
 
-  def when_successful_registered
-    flash[:notice] = 'User registered! Please, confirm email'
+  def when_email_taken
+    flash[:notice] = 'User with this email already registered. Please try to login'
     redirect_to new_user_session_path
   end
 
