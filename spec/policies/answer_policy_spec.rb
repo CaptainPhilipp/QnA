0bfef588c2b07da3f1f46790be9fd9bd79f9aa59
CaptainPhilipp <Ph-s@mail.ru>
2017-06-27 01:38:18 +0300

    should-style specs

--- spec/policies/answer_policy_spec.rb
+++ spec/policies/answer_policy_spec.rb
@@ -6,23 +6,32 @@ RSpec.describe AnswerPolicy do
 
   subject { described_class }
 
-  permissions ".scope" do
-    pending "add some examples to (or delete) #{__FILE__}"
+  permissions :create?, :new? do
+    it { should_not permit guest, Answer }
+    it { should     permit user,  Answer }
   end
 
-  permissions :show? do
-    pending "add some examples to (or delete) #{__FILE__}"
+  permissions :index?, :show? do
+    it { should     permit guest,      answer }
+    it { should     permit user,       answer }
+    it { should     permit other_user, answer }
   end
 
-  permissions :create? do
-    pending "add some examples to (or delete) #{__FILE__}"
+  permissions :update?, :edit?, :destroy? do
+    it { should_not permit guest,      answer }
+    it { should     permit user,       answer }
+    it { should_not permit other_user, answer }
   end
 
-  permissions :update? do
-    pending "add some examples to (or delete) #{__FILE__}"
+  permissions :best? do
+    it { should_not permit guest,      answer }
+    it { should_not permit user,       answer }
+    it { should     permit other_user, answer }
   end
 
-  permissions :destroy? do
-    pending "add some examples to (or delete) #{__FILE__}"
+  permissions :vote? do
+    it { should_not permit guest,      answer }
+    it { should_not permit user,       answer }
+    it { should     permit other_user, answer }
   end
 end
