a86af1b05dfae8dea5ca87d2ce1e9dc59209951f
CaptainPhilipp <CaptainPhilipp@users.noreply.github.com>
2017-06-27 11:46:35 +0300

    respond when unauthorized

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,22 +2,20 @@ require "application_responder"
 class ApplicationController < ActionController::Base
   include Pundit
   include ApplicationHelper
-
   protect_from_forgery with: :exception
 
   self.responder = ApplicationResponder
   respond_to :html, :js, :json
 
   before_action :gon_current_user
-
   rescue_from Pundit::NotAuthorizedError, with: :user_not_authorized
 
   private
 
   def user_not_authorized
     respond_to do |format|
-      format.js   { head :unauthorized, content_type: 'text/html' }
-      format.json { head :unauthorized, content_type: 'text/html' }
+      format.js   { head :unauthorized }
+      format.json { head :unauthorized }
       format.html { redirect_to root_url, notice: exception.message }
     end
   end
