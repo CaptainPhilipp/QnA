5dde4880ac088f0e744b1331b2672c602775f103
CaptainPhilipp <Ph-s@mail.ru>
2017-06-27 10:28:33 +0300

    sooo

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,24 +1,25 @@
 require "application_responder"
-include Pundit
 class ApplicationController < ActionController::Base
+  protect_from_forgery with: :exception
   include Pundit
   include ApplicationHelper
-  protect_from_forgery with: :exception
+
+  rescue_from Pundit::NotAuthorizedError, with: :user_not_authorized
 
   self.responder = ApplicationResponder
   respond_to :html, :js, :json
 
   before_action :gon_current_user
 
-  rescue_from Pundit::NotAuthorizedError do
+  private
+
+  def user_not_authorized
     respond_to do |format|
-      format.js   { self.status = :unauthorized }
-      format.json { self.status = :unauthorized }
-      format.html { redirect_to root_path }
+      format.js   { head :unauthorized, content_type: 'text/html' }
+      format.json { head :unauthorized, content_type: 'text/html' }
+      format.html { redirect_to root_url, notice: exception.message }
     end
   end
-
-  private
 
   def gon_current_user
     gon.current_user_id = current_user.id if current_user
