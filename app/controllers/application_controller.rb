572e646c27527cd8d1aa8c8e896d6ae97a505326
CaptainPhilipp <Ph-s@mail.ru>
2017-06-27 09:13:17 +0300

    respond status

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,12 +6,16 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
 
   self.responder = ApplicationResponder
-  respond_to :html
+  respond_to :html, :js, :json
 
   before_action :gon_current_user
 
   rescue_from Pundit::NotAuthorizedError do
-    redirect_to root_path
+    respond_to do |format|
+      format.js   { self.status = :unauthorized }
+      format.json { self.status = :unauthorized }
+      format.html { redirect_to root_path }
+    end
   end
 
   private
