789ef3b04d0af666f3cfdb024eaf5c8b0e48ed84
CaptainPhilipp <Ph-s@mail.ru>
2017-07-09 22:10:58 +0300

    subscriptions raw controller with specs

---
+++ spec/controllers/subscriptions_controller_spec.rb
@@ -0,0 +1,30 @@
+require 'rails_helper'
+
+RSpec.describe SubscriptionsController, type: :controller do
+  login_user
+
+  describe "POST #create" do
+    let(:question) { create :question }
+    let(:send_request) { post :create, params: { question_id: question.id } }
+
+    it 'creates subscription' do
+      expect { send_request }.to change(Subscription, :count).by(1)
+    end
+
+    it 'created subscription have right user and question' do
+      send_request
+      subscription = Subscription.last
+      expect(subscription.user).to eq user
+      expect(subscription.question).to eq question
+    end
+  end
+
+  describe "DELETE #destroy" do
+    let!(:subscription) { create :subscription }
+    let(:send_request) { delete :destroy, params: { id: subscription.id } }
+
+    it 'deletes subscription' do
+      expect { send_request }.to change(Subscription, :count).by(-1)
+    end
+  end
+end
