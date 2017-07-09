34b752100cc76601ff673057db036f9e5e48230e
CaptainPhilipp <Ph-s@mail.ru>
2017-07-09 18:46:58 +0300

    mailer spec

--- spec/mailers/daily_spec.rb
+++ spec/mailers/daily_spec.rb
@@ -1,18 +1,23 @@
-require "rails_helper"
+require 'rails_helper'
 
 RSpec.describe DailyMailer, type: :mailer do
-  describe "digest" do
-    let(:mail) { DailyMailer.digest }
+  assign_user
 
-    it "renders the headers" do
-      expect(mail.subject).to eq("Digest")
-      expect(mail.to).to eq(["to@example.org"])
-      expect(mail.from).to eq(["from@example.com"])
+  describe '#digest' do
+    let(:mail) { DailyMailer.digest(user) }
+    let!(:users_questions) { create :question, user: user }
+    let!(:questions) { create_list :question, 3 }
+
+    it 'renders the headers' do
+      expect(mail.subject).to eq('Daily digest')
+      expect(mail.to).to eq([user.email])
+      expect(mail.from).to eq(['from@example.com'])
     end
 
-    it "renders the body" do
-      expect(mail.body.encoded).to match("Hi")
+    it 'body have new questions' do
+      questions.each do |question|
+        expect(mail.body.encoded).to have_content(question.title)
+      end
     end
   end
-
 end
