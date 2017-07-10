cf4b8f3d852165c34fa62fefc0efac757e4943f4
CaptainPhilipp <Ph-s@mail.ru>
2017-07-10 09:15:43 +0300

    subscription policy

--- app/controllers/subscriptions_controller.rb
+++ app/controllers/subscriptions_controller.rb
@@ -1,4 +1,7 @@
 class SubscriptionsController < ApplicationController
+  before_action :autrhorize_subscriptions
+  after_action  :verify_authorized
+
   def create
     Subscription.find_or_create_by subscription_arguments
     redirect_to question_path(question_id)
@@ -10,6 +13,10 @@ class SubscriptionsController < ApplicationController
   end
 
   private
+
+  def autrhorize_subscriptions
+    authorize Subscription
+  end
 
   def subscription_arguments
     { question_id: question_id, user: user }
