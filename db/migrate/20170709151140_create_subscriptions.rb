de85f1ef7bc845ee76816dc45dc21499ba47b3e1
CaptainPhilipp <Ph-s@mail.ru>
2017-07-11 20:55:13 +0300

    add double index to subscriptions

--- db/migrate/20170709151140_create_subscriptions.rb
+++ db/migrate/20170709151140_create_subscriptions.rb
@@ -3,6 +3,7 @@ class CreateSubscriptions < ActiveRecord::Migration[5.0]
     create_table :subscriptions do |t|
       t.references :question, foreign_key: true, null: false
       t.references :user, foreign_key: true, null: false
+      t.index %i[question_id user_id]
 
       t.timestamps
     end
