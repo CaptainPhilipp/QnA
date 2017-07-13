f5de710e5dd8dadff2a3d24a96b9a9b0df65e4c0
CaptainPhilipp <Ph-s@mail.ru>
2017-07-13 20:55:11 +0300

    multitype search

--- spec/features/searchings_spec.rb
+++ spec/features/searchings_spec.rb
@@ -1,19 +1,45 @@
 require_relative 'acceptance_helper'
 
-RSpec.feature "Searching", type: :feature do
+RSpec.feature "Searching", type: :feature, sphinx: true do
   let(:query) { 'Searcheable' }
-  let(:body) { "Body #{query}" }
-  let(:question_with_title) { create :question, title: "Title #{query}" }
-  let(:question_with_body)  { create :question, body: body }
-  let(:answer_with_body)    { create :answer,   body: body }
-  let(:comment_with_body)   { create :comment,  body: body }
 
-  fcontext 'with all types of content' do
-    before { visit root_path }
+  before { visit root_path }
+
+  context 'with all types of content' do
+    let(:body) { "Body #{query}" }
+    let(:question_with_title) { create :question, title: "Title #{query}" }
+    let(:question_with_body)  { create :question, body: body }
+    let(:answer_with_body)    { create :answer,   body: body }
+    let(:comment_with_body)   { create :comment,  body: body }
 
     it_behaves_like 'sends search request', entity: :question_with_title
     it_behaves_like 'sends search request', entity: :question_with_body
     it_behaves_like 'sends search request', entity: :answer_with_body
     it_behaves_like 'sends search request', entity: :comment_with_body
+  end
+
+  fcontext 'with a few types of content' do
+    let!(:question) { create :question, body: "#{query} KeywordQuestion" }
+    let!(:answer)   { create :answer,   body: "#{query} KeywordAnswer" }
+    let!(:comment)  { create :comment,  body: "#{query} KeywordComment" }
+
+    before { index }
+
+    scenario 'tryes to find Answer within Question type' do
+      within '#search_form' do
+        fill_in 'query', with: query
+
+        within('#select_type') do
+          check 'Question'
+          check 'Comment'
+        end
+
+        click_on 'Search!'
+      end
+
+      expect(page).to have_content 'KeywordQuestion'
+      expect(page).to have_content 'KeywordComment'
+      expect(page).to_not have_content 'KeywordAnswer'
+    end
   end
 end
